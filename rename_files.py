22ab2694378999cae06c6cc14af0fefc4a15f80f
cameron-yee <yee.camero23@gmail.com>
2018-06-08 14:17:43 -0600

    Added punctuation removal

--- rename_files.py
+++ rename_files.py
@@ -1,19 +1,29 @@
 import sys
 import os
 from glob import glob
+import string
 
 def inp():
     f = input('File Path: ')
     return f
 
 
+def stripPunctuation(name):
+    x = string.punctuation.replace('_','')
+    punct = x.replace('.','')
+    table = str.maketrans('', '', punct)
+    final_name = name.translate(table)
+    return final_name
+
+
 def renameFile(f):
-    b = os.path.basename(f)
-    l = b.lower()
-    n = l.replace(' ','_') 
-    d = os.path.dirname(f)
+    base = os.path.basename(f)
+    base_lower = base.lower()
+    name = base_lower.replace(' ','_') 
+    final_name = stripPunctuation(name)
+    dirname = os.path.dirname(f)
     sf = str(f)
-    sdn = str(d + '/' + n)
+    sdn = str(dirname + '/' + final_name)
     os.rename(sf,sdn)
 
 
@@ -27,6 +37,8 @@ def getFilePaths(directory):
 if __name__ == '__main__':
     #f = inp()
     #directory = '/Users/cyee/Desktop/pdf_concat/mnstl_elm_segment_7/lesson_1'
+    n = '1'
+    stripPunctuation(n)
     directory = os.getcwd()
     files = getFilePaths(directory)
     for f in files:
