ac32e2ea78fcc2dc98da7706da4549d5151f402b
cameron-yee <yee.camero23@gmail.com>
2018-08-06 14:57:53 -0600

    Added script to run in current directory when ran as executable

---
+++ rename_files_cl.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python3
+import sys
+import os
+from glob import glob
+import string
+import re
+import time
+
+def stripPunctuation(name):
+    x = string.punctuation.replace('_','')
+    punct = x.replace('.','')
+    table = str.maketrans('', '', punct)
+    final_name = name.translate(table)
+    return final_name
+
+
+def removeDoubleFileType(name_2):
+    final_name = re.sub('docx.pdf$','pdf', name_2)   
+    return final_name
+
+
+def renameFile(f, count):
+    try:
+        base = os.path.basename(f)
+        base_lower = base.lower()
+        name = base_lower.replace(' ','_') 
+        name_2 = stripPunctuation(name)
+
+        final_name = removeDoubleFileType(name_2)
+
+        dirname = os.path.dirname(f)
+        sf = str(f)
+        sdn = str(dirname + '/' + final_name)
+        os.rename(sf,sdn)
+    except(FileNotFoundError):
+        if count < 500:
+            time.sleep(10)
+            count += 1
+            renameFile(f, count)
+        else:
+            print('Download took too long to rename.  Possible download failure.')
+
+
+def getFilePaths(directory):
+    files = []
+    dir_files = os.listdir(directory)
+    relevant_files = [f for f in dir_files if f not in ['.rename_files.py', '.DS_Store', '.localized']]
+
+    for f in relevant_files:
+        files.append(directory + '/{}'.format(f))
+
+    return files
+
+
+if __name__ == '__main__':
+    directory = sys.argv[1]
+    files = getFilePaths(directory)
+    count = 0
+    for f in files:
+        renameFile(f, count)
