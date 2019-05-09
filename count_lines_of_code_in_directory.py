588b4b62bfe4d8768adee2d4ed405bb43e1ab8c2
cameron-yee <yee.camero23@gmail.com>
2019-05-09 14:15:50 -0600

    Added script to count lines of code in a directory either recursive or not

---
+++ count_lines_of_code_in_directory.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+from glob import glob
+import sys
+import os
+
+def getFilePaths(directory, file_extension):
+    files = glob('{}*.{}'.format(directory, file_extension))
+    return files
+
+
+def getFilePathsRecursive(directory, file_extension):
+    files = glob('{}**/*.{}'.format(directory, file_extension), recursive=True)
+    return files
+
+
+def countLines(files):
+    lines = []
+
+    for fil in files:
+        with open(fil, 'r') as f:
+            file_lines = f.readlines()
+            lines.extend(file_lines)
+
+    print('{} lines'.format(len(lines)))
+
+
+if __name__ == '__main__':
+    directory = sys.argv[1] 
+    file_extension = sys.argv[2]
+    recursive = sys.argv[3] == 'True'
+
+    files = None
+    if recursive:
+        files = getFilePathsRecursive(directory, file_extension)
+    else:
+        files = getFilePaths(directory, file_extension)
+
+    countLines(files)
